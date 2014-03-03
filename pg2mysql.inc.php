40a5805271780aa124c90a844da746558f31b7e7
John Supplee <john@supplee.com>
2014-03-03 15:39:26 +0200

    add UNIQUE constraints

--- pg2mysql.inc.php
+++ pg2mysql.inc.php
@@ -335,8 +335,8 @@ function pg2mysql(&$input, $header=true)
 			$linenumber++;
 			$line=$lines[$linenumber];
 
-			if(strstr($line," PRIMARY KEY ") && substr($line,-3,-1)==");") {
-				//looks like we have a single line PRIMARY KEY definition, lets go ahead and add it
+			if(preg_match('/(PRIMARY KEY|UNIQUE).*\);\s*$/', $line)) {
+				//looks like we have a single line PRIMARY KEY or UNIQUE definition, lets go ahead and add it
 				$output.=$pkey;
 				//the postgres and mysql syntax for this is (at least, in the example im looking at)
 				//identical, so we can just add it as is.
